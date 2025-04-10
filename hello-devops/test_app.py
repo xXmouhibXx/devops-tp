23b9a4f5cdc28cd8e704f3971e6e1cac2b4c6a4e
xXmouhibXx <156907546+xXmouhibXx@users.noreply.github.com>
2025-04-10 15:41:54 +0100

    Add GitLab CI config

--- hello-devops/test_app.py
+++ hello-devops/test_app.py
@@ -1,10 +1,11 @@
 # test_app.py
 import unittest
+from app import app
 
-# For a simple example, we simply test True.
 class TestApp(unittest.TestCase):
-    def test_output(self):
-        self.assertTrue(True)
-
+    def test_home_route(self):
+        tester = app.test_client(self)
+        response = tester.get('/')
+        self.assertEqual(response.status_code, 200)
 if __name__ == "__main__":
     unittest.main()
